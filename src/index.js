96e26c6ebe60153b8d69521e5b89bb043d1c6286
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-09-04 13:33:09 -0300

    atualizado para jeito mais simples de middleware

--- src/index.js
+++ src/index.js
@@ -13,29 +13,29 @@ const users = [];
 function checksExistsUserAccount(req, res, next) {
   const { username } = req.headers;
 
-  const userExists = users.some(user => user.username === username);
+  const user = users.find(user => user.username === username);
 
-  if (!userExists) {
-    return res.status(400).json({ error: "Usuário não existe!" });
+  if(!user){
+    return res.status(400).json({error: "Usuário não existe!"});
   }
 
-  req.username = username;
+  req.user = user;
   return next();
 }
 
 app.post('/users', (req, res) => {
   const { name, username } = req.body;
+  const userAlreadyExist = users.some(user => user.username === username);
+
+  if (userAlreadyExist) {
+    return res.status(400).json({error: "Usuário já existe!"});
+  };
+
   const user = {
     id: uuidv4(),
     name,
     username,
     todos: []
-  };
-
-  const userAlreadyExist = users.some(user => user.username === username);
-
-  if (userAlreadyExist) {
-    return res.status(400).json({error: "Usuário já existe!"});
   };
 
   users.push(user);
@@ -44,18 +44,14 @@ app.post('/users', (req, res) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (req, res) => {
-  const { username } = req;
+  const { user } = req;
 
-  const user = users.find(user => user.username === username);
-
-  return res.json(user.todos);
+  return res.json(user);
 });
 
 app.post('/todos', checksExistsUserAccount, (req, res) => {
-  const { username } = req;
+  const { user } = req;
   const { title, deadline } = req.body;
-
-  const user = users.find(user => user.username === username);
 
   const task = {
     id: uuidv4(),
@@ -71,11 +67,9 @@ app.post('/todos', checksExistsUserAccount, (req, res) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
-  const { username } = req;
+  const { user } = req;
   const { title, deadline } = req.body;
   const { id } = req.params;
-
-  const user = users.find(user => user.username === username);
 
   const task = user.todos.find(todo => todo.id === id);
 
@@ -94,10 +88,8 @@ app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
-  const { username } = req;
+  const { user } = req;
   const { id } = req.params;
-
-  const user = users.find(user => user.username === username);
 
   const task = user.todos.find(todo => todo.id === id);
 
@@ -111,10 +103,8 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (req, res) => {
-  const { username } = req;
+  const { user } = req;
   const { id } = req.params;
-
-  const user = users.find(user => user.username === username);
 
   const task = user.todos.find(todo => todo.id === id);
 
